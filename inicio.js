5d64a5635a1935cf708380ae4bd78bae0344cc0c
javierenrique00@hotmail.com <javierenrique00@hotmail.com>
2019-05-05 19:36:29 -0500

    Testing with Amazon S3

--- inicio.js
+++ inicio.js
@@ -2,41 +2,61 @@ var Kefir = require("kefir")
 var Minio = require('minio')
 const miModulo = require("./indexServer")
 
+const S3 = true  //--- false for minio, true fo Amazon S3
 
-//--- Configuring Globals
-var bucket = "test"                     //--- name of the bucket
-var pathMusic = "music/"                //--- path to the music library that you want to index
-var indexFileName = "music.index"       //--- leave a music.index (default)
+// //--- Configuring Globals USE for minio
+// var bucket = "test"                     //--- name of the bucket
+// var pathMusic = "music/"                //--- path to the music library that you want to index
+// var indexFileName = "music.index"       //--- leave a music.index (default)
+// var minioClient = new Minio.Client({
+//     endPoint: '192.168.0.8',            //--- IP of the Minio Music Server where the music library lives
+//     port: 9000,                         //--- Port of the Minio server (9000 is default)
+//     useSSL: false,                      //--- without SSL, put true for SSL access
+//     accessKey: 'admin',                 //---  Minio server Access key
+//     secretKey: 'password'               //---  Minio server Secret Key
+// });
+
+
+//--- Configuring Globals USE for S3  Comment in case you have Minio
+var bucket = "mipublico"                     //--- name of the bucket
+var pathMusic = "music/"                     //--- path to the music library that you want to index
+var indexFileName = "music.index"            //--- leave a music.index (default)
 var minioClient = new Minio.Client({
-    endPoint: '192.168.0.8',            //--- IP of the Minio Music Server where the music library lives
-    port: 9000,                         //--- Port of the Minio server (9000 is default)
-    useSSL: false,                      //--- without SSL, put true for SSL access
-    accessKey: 'admin',                 //---  Minio server Access key
-    secretKey: 'password'               //---  Minio server Secret Key
+    endPoint: 's3.amazonaws.com',            //--- IP of Amazon
+    accessKey: 'XXXXXXXXXXX',       //---  Amazon server Access key
+    secretKey: 'XXXXXXXXXXXXXXXXXXXXX'   //---  Amazon Secret Key
 });
 
-
-//--- Crea el stream minioEvents$ cuando hay eventos de creación y eliminación de objetos
+if(S3){
+        //--- se ejecuta cuando es S3
+        let exec=miModulo.moduleIndex(bucket,pathMusic,indexFileName,minioClient)
+}
+else{
+        //--- para minio
+        //--- Crea el stream minioEvents$ cuando hay eventos de creación y eliminación de objetos
 var minioEvents$ = Kefir.stream( emmiter => {
-    var listener = minioClient.listenBucketNotification(bucket, 'music/', '.mp3', ['s3:ObjectCreated:*','s3:ObjectRemoved:*'])
-    listener.on('notification', function(record) {
-        console.log('%s event occurred (%s)', record.eventName, record.eventTime)
-        emmiter.emit(1)
-        //---listener.stop()
+        var listener = minioClient.listenBucketNotification(bucket, 'music/', '.mp3', ['s3:ObjectCreated:*','s3:ObjectRemoved:*'])
+        listener.on('notification', function(record) {
+            console.log('%s event occurred (%s)', record.eventName, record.eventTime)
+            emmiter.emit(1)
+            //---listener.stop()
+        })
+        
     })
     
-})
+    
+    //-- Debe invocar la recreación de un nuevo indice después de que pase 30 segundos sin hacer cambios a la estructura de archivos
+    var disable$ = minioEvents$
+                .debounce(20000)
+                .onValue( x =>{
+                        console.log("--Activando indice--")
+    
+                        let exec=miModulo.moduleIndex(bucket,pathMusic,indexFileName,minioClient);
+    
+                })
 
+}
 
-//-- Debe invocar la recreación de un nuevo indice después de que pase 30 segundos sin hacer cambios a la estructura de archivos
-var disable$ = minioEvents$
-            .debounce(20000)
-            .onValue( x =>{
-                    console.log("--Activando indice--")
-
-                    let exec=miModulo.moduleIndex(bucket,pathMusic,indexFileName,minioClient);
-
-            })
 
 
 
