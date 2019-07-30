7750da66621c9b0b4eb6bbec0f79e6738ef888f3
javierenrique00@hotmail.com <javierenrique00@hotmail.com>
2019-07-30 15:33:10 -0500

    Testing configurartion

--- inicio.js
+++ inicio.js
@@ -12,32 +12,33 @@ const minio = false  //--- true for minio, false for Amazon S3 or a minio gatewa
 var SCAN_METADATA = true //--- false for only read the basic data from directory listing, no metadata but is very fast because doesn't need to read all files for extracting metadata
                            //--- true for read all file and extract metadata information
 const ENCRYPTED = true    //--- True for encrypted index database, False no encryption
-var PASSWORD = "xxx" //----Choose a password
+var PASSWORD = "XXXXXXXXXXXXX" //----Choose a password
+const keyFileName = "key"
 
 //--- Configuring Globals USE for minio on Google Cloud with minio gateway
-// var bucket = "bogota2"                     //--- name of the bucket
-// var pathMusic = "data/"                //--- path to the music library that you want to index
-// var indexFileName = "crypt.index"       //--- leave a music.index (default)
-// var minioClient = new Minio.Client({
-//     endPoint: '192.168.0.16',            //--- IP of the Minio Music Server where the music library lives
-//     port: 9000,                         //--- Port of the Minio server (9000 is default)
-//     useSSL: false,                      //--- without SSL, put true for SSL access
-//     accessKey: 'GOOGBPT6F7Y2KKVMG3KOFXTO',                 //---  Minio server Access key
-//     secretKey: '5wIW5p5MgRD7GHXsu6OMckDcBA1fZrp3Ya4+3zZ2'       //---  Minio server Secret Key
-// });
-
-//--- Configuring Globals USE for minio  Confioguraciona para acceso a Minio server local
-var bucket = "cri"                     //--- name of the bucket
-var pathMusic = "music/"                //--- path to the music library that you want to index
-var indexFileName = "crypt.index"         //---Index file name
-var keyFileName = "key"                   //--- Key File name
+var bucket = "bogota2"                     //--- name of the bucket
+var pathMusic = "data/"                //--- path to the music library that you want to index
+var indexFileName = "crypt.index"       //--- leave a music.index (default)
 var minioClient = new Minio.Client({
-    endPoint: '192.168.0.8',            //--- IP of the Minio Music Server where the music library lives
+    endPoint: '192.168.0.16',            //--- IP of the Minio Music Server where the music library lives
     port: 9000,                         //--- Port of the Minio server (9000 is default)
     useSSL: false,                      //--- without SSL, put true for SSL access
-    accessKey: 'admin',                 //---  Minio server Access key
-    secretKey: 'password'       //---  Minio server Secret Key
+    accessKey: 'XXXXXXXXXXXXXXXXXXXXXXX',                 //---  Minio server Access key
+    secretKey: 'XXXXXXXXXXXXXXXXXXXXX'       //---  Minio server Secret Key
 });
+
+//--- Configuring Globals USE for minio  Confioguraciona para acceso a Minio server local
+// var bucket = "cri"                     //--- name of the bucket
+// var pathMusic = "music/"                //--- path to the music library that you want to index
+// var indexFileName = "crypt.index"         //---Index file name
+// var keyFileName = "key"                   //--- Key File name
+// var minioClient = new Minio.Client({
+//     endPoint: '192.168.0.8',            //--- IP of the Minio Music Server where the music library lives
+//     port: 9000,                         //--- Port of the Minio server (9000 is default)
+//     useSSL: false,                      //--- without SSL, put true for SSL access
+//     accessKey: 'admin',                 //---  Minio server Access key
+//     secretKey: 'password'       //---  Minio server Secret Key
+// });
 
 //--- Configuring Globals USE for S3  Comment in case you have Minio
 // var bucket = "mipublico"                     //--- name of the bucket
